cb04a5d5400584b6455a7f5674474c7e840ae3f8
setra2014 <36576562+setra2014@users.noreply.github.com>
2019-07-15 14:12:33 +0300

    Update token_stream.h

--- token_stream.h
+++ token_stream.h
@@ -1,13 +1,95 @@
 #pragma once
-#include "token.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <complex>
+#include "calculate.h"
+#include "constName.h"
+#include "Variable.h"
+#include "Token.h"
 
+using namespace std;
+using namespace ConstName;
+template <typename T>
 class Token_stream {
 public:
 	Token_stream() : full{ false } {}
-	Token get();
-	void putback(Token t);
-	void ignore(char c);
+	template <typename T> Token <T> get() {
+		if (full) {
+			full = false;
+			return buffer;
+		}
+		char ch;
+		cin >> ch;
+
+		switch (ch) {
+		case print:
+		case quit:
+		case help:
+		case image:
+		case '(': case ')':
+		case '{': case '}':
+		case '+': case '-':
+		case '!':
+		case '*': case '/': case '%':
+		case '=':
+			return Token<T>(ch);
+		case '.':
+		case 'О': case '1': case '2':
+		case '3': case '4': case '5':
+		case '6': case '7': case '8':
+		case '9':
+		{
+			cin.putback(ch);
+			T c;
+			cin >> c;
+			return Token<T>(number, c);
+		}
+		default: {
+			if (isalpha(ch)) {
+				string s;
+				s += ch;
+				bool c = false;
+				while (cin.get(ch) && (isalpha(ch) || isdigit(ch) || ch == '_')) s += ch;
+				cin.putback(ch);
+				if (s == what_const) {
+					c = true;
+					return Token<T>{ cons };
+				}
+				if (s == declkey)
+					return Token<T>{ let };
+				return Token<T>{ name, s, c };
+			}
+		}
+				 error("Неверная лексема");
+
+		}
+	}
+
+	template <typename T> void putback(Token <T> t) {
+		if (full) error("put back() : буфер заполнен");
+		buffer = t;
+		full = true;
+	}
+	template <typename T> void ignore(char c) {
+		if (full && c == buffer.kind) {
+			full = false;
+			return;
+		}
+		full = false;
+
+		// проверяем входные данные
+		char ch;
+		while (cin >> ch)
+			if (ch == c) return;
+	}
+	void clear() {
+		full = false;
+		if(buffer.kind == number)
+			cin.putback(buffer.value);
+	}
 private:
 	bool full{ false };
-	Token buffer;
+	Token <T> buffer;
 };
